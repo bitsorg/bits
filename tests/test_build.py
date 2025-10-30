alisw/alibuild

Fix darwin tests in new macos
zlib<1.3.1 doesn't build in the newer XCode. I'm bumping the zlib
version and fixing the architecture for the macOS ARM tests in general

tests/test_build.py
@@ -1,6 +1,7 @@
 from argparse import Namespace
 import os
 import os.path
+import platform
 import re
 import sys
 import unittest
@@ -11,6 +12,19 @@ from collections import OrderedDict
 
 from alibuild_helpers.utilities import parseRecipe, resolve_tag
 from alibuild_helpers.build import doBuild, storeHashes, generate_initdotsh
+
+# Determine architecture based on platform
+def get_test_architecture():
+    if sys.platform == 'darwin':
+        machine = platform.machine()
+        if machine == 'arm64':
+            return 'osx_arm64'
+        else:
+            return 'osx_x86-64'
+    else:
+        return 'slc7_x86-64'
+
+TEST_ARCHITECTURE = os.environ.get('ARCHITECTURE', get_test_architecture())
 
 
 TEST_DEFAULT_RELEASE = """\
@@ -23,7 +37,7 @@ TEST_DEFAULT_RELEASE_BUILD_HASH = "27ce49698e818e8efb56b6eff6dd785e503df341"
 
 TEST_ZLIB_RECIPE = """\
 package: zlib
-version: v1.2.3
+version: v1.3.1
 source: https://github.com/madler/zlib
 tag: master
 ---
@@ -32,7 +46,7 @@ make
 make install
 """
 TEST_ZLIB_GIT_REFS = "8822efa61f2a385e0bc83ca5819d608111b2168a\trefs/heads/master"
-TEST_ZLIB_BUILD_HASH = "8cd1f56c450f05ffbba3276bad08eae30f814999"
+TEST_ZLIB_BUILD_HASH = "4d6a75f214dc7931a2a7d5ba82ea0568e652cd84"
 
 TEST_ROOT_RECIPE = """\
 package: ROOT
@@ -71,7 +85,7 @@ TEST_ROOT_GIT_REFS = """\
 87b87c4322d2a3fad315c919cb2e2dd73f2154dc\trefs/heads/master
 f7b336611753f1f4aaa94222b0d620748ae230c0\trefs/heads/v6-08-00-patches
 f7b336611753f1f4aaa94222b0d620748ae230c0\trefs/tags/test-tag"""
-TEST_ROOT_BUILD_HASH = ("8ec3f41b6b585ef86a02e9c595eed67f34d63f08")
+TEST_ROOT_BUILD_HASH = ("1f3c771080f71b6c0d2e3d7a285698a20035da12")
 
 
 TEST_EXTRA_RECIPE = """\
@@ -88,18 +102,18 @@ f000\trefs/heads/master
 ba22\trefs/tags/v1
 ba22\trefs/tags/v2
 baad\trefs/tags/v3"""
-TEST_EXTRA_BUILD_HASH = ("5afae57bfc6a374e74c1c4427698ab5edebce0bc")
+TEST_EXTRA_BUILD_HASH = ("6e7bc4976abf77b558cf7faf575ec51670f8d0e5")
 
 
 GIT_CLONE_REF_ZLIB_ARGS = ("clone", "--bare", "https://github.com/madler/zlib",
                            "/sw/MIRROR/zlib", "--filter=blob:none"), ".", False
 GIT_CLONE_SRC_ZLIB_ARGS = ("clone", "-n", "https://github.com/madler/zlib",
-                           "/sw/SOURCES/zlib/v1.2.3/8822efa61f",
+                           "/sw/SOURCES/zlib/v1.3.1/8822efa61f",
                            "--dissociate", "--reference", "/sw/MIRROR/zlib", "--filter=blob:none"), ".", False
 GIT_SET_URL_ZLIB_ARGS = ("remote", "set-url", "--push", "origin", "https://github.com/madler/zlib"), \
-    "/sw/SOURCES/zlib/v1.2.3/8822efa61f", False
+    "/sw/SOURCES/zlib/v1.3.1/8822efa61f", False
 GIT_CHECKOUT_ZLIB_ARGS = ("checkout", "-f", "master"), \
-    "/sw/SOURCES/zlib/v1.2.3/8822efa61f", False
+    "/sw/SOURCES/zlib/v1.3.1/8822efa61f", False
 
 GIT_FETCH_REF_ROOT_ARGS = ("fetch", "-f", "--prune", "--filter=blob:none", "https://github.com/root-mirror/root", "+refs/tags/*:refs/tags/*",
                            "+refs/heads/*:refs/heads/*"), "/sw/MIRROR/root", False
@@ -143,9 +157,9 @@ def dummy_open(x, mode="r", encoding=None, errors=None):
                 "/sw/BUILD/%s/defaults-release/.build_succeeded" % TEST_DEFAULT_RELEASE_BUILD_HASH: (0, StringIO("0")),
                 "/sw/BUILD/%s/zlib/.build_succeeded" % TEST_ZLIB_BUILD_HASH: (0, StringIO("0")),
                 "/sw/BUILD/%s/ROOT/.build_succeeded" % TEST_ROOT_BUILD_HASH: (0, StringIO("0")),
-                "/sw/osx_x86-64/defaults-release/v1-1/.build-hash": (1, StringIO(TEST_DEFAULT_RELEASE_BUILD_HASH)),
-                "/sw/osx_x86-64/zlib/v1.2.3-local1/.build-hash": (1, StringIO(TEST_ZLIB_BUILD_HASH)),
-                "/sw/osx_x86-64/ROOT/v6-08-30-local1/.build-hash": (1, StringIO(TEST_ROOT_BUILD_HASH))
+                f"/sw/{TEST_ARCHITECTURE}/defaults-release/v1-1/.build-hash": (1, StringIO(TEST_DEFAULT_RELEASE_BUILD_HASH)),
+                f"/sw/{TEST_ARCHITECTURE}/zlib/v1.3.1-local1/.build-hash": (1, StringIO(TEST_ZLIB_BUILD_HASH)),
+                f"/sw/{TEST_ARCHITECTURE}/ROOT/v6-08-30-local1/.build-hash": (1, StringIO(TEST_ROOT_BUILD_HASH))
             }[x]
         except KeyError:
             return DEFAULT
@@ -163,17 +177,16 @@ def dummy_execute(x, **kwds):
     if re.match(".*ln -sfn.*TARS", s):
         return 0
     return {
-        "/bin/bash -e -x /sw/SPECS/osx_x86-64/defaults-release/v1-1/build.sh 2>&1": 0,
-        '/bin/bash -e -x /sw/SPECS/osx_x86-64/zlib/v1.2.3-local1/build.sh 2>&1': 0,
-        '/bin/bash -e -x /sw/SPECS/osx_x86-64/ROOT/v6-08-30-local1/build.sh 2>&1': 0,
+        f"/bin/bash -e -x /sw/SPECS/{TEST_ARCHITECTURE}/defaults-release/v1-1/build.sh 2>&1": 0,
+        f'/bin/bash -e -x /sw/SPECS/{TEST_ARCHITECTURE}/zlib/v1.3.1-local1/build.sh 2>&1': 0,
+        f'/bin/bash -e -x /sw/SPECS/{TEST_ARCHITECTURE}/ROOT/v6-08-30-local1/build.sh 2>&1': 0,
     }[s]
 
 
 def dummy_readlink(x):
     return {
-        "/sw/TARS/osx_x86-64/defaults-release/defaults-release-v1-1.osx_x86-64.tar.gz":
-        "../../osx_x86-64/store/%s/%s/defaults-release-v1-1.osx_x86-64.tar.gz" %
-        (TEST_DEFAULT_RELEASE_BUILD_HASH[:2], TEST_DEFAULT_RELEASE_BUILD_HASH)
+        f"/sw/TARS/{TEST_ARCHITECTURE}/defaults-release/defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz":
+        f"../../{TEST_ARCHITECTURE}/store/{TEST_DEFAULT_RELEASE_BUILD_HASH[:2]}/{TEST_DEFAULT_RELEASE_BUILD_HASH}/defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz"
     }[x]
 
 
@@ -224,13 +237,11 @@ class BuildTestCase(unittest.TestCase):
     @patch("os.listdir")
     @patch("alibuild_helpers.build.glob", new=lambda pattern: {
         "*": ["zlib"],
-        "/sw/TARS/osx_x86-64/store/%s/%s/*gz" % (TEST_DEFAULT_RELEASE_BUILD_HASH[:2],
-                                                 TEST_DEFAULT_RELEASE_BUILD_HASH): [],
-        "/sw/TARS/osx_x86-64/store/%s/%s/*gz" % (TEST_ZLIB_BUILD_HASH[:2], TEST_ZLIB_BUILD_HASH): [],
-        "/sw/TARS/osx_x86-64/store/%s/%s/*gz" % (TEST_ROOT_BUILD_HASH[:2], TEST_ROOT_BUILD_HASH): [],
-        "/sw/TARS/osx_x86-64/defaults-release/defaults-release-v1-1.osx_x86-64.tar.gz":
-        ["../../osx_x86-64/store/%s/%s/defaults-release-v1-1.osx_x86-64.tar.gz" %
-         (TEST_DEFAULT_RELEASE_BUILD_HASH[:2], TEST_DEFAULT_RELEASE_BUILD_HASH)],
+        f"/sw/TARS/{TEST_ARCHITECTURE}/store/{TEST_DEFAULT_RELEASE_BUILD_HASH[:2]}/{TEST_DEFAULT_RELEASE_BUILD_HASH}/*gz": [],
+        f"/sw/TARS/{TEST_ARCHITECTURE}/store/{TEST_ZLIB_BUILD_HASH[:2]}/{TEST_ZLIB_BUILD_HASH}/*gz": [],
+        f"/sw/TARS/{TEST_ARCHITECTURE}/store/{TEST_ROOT_BUILD_HASH[:2]}/{TEST_ROOT_BUILD_HASH}/*gz": [],
+        f"/sw/TARS/{TEST_ARCHITECTURE}/defaults-release/defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz":
+        [f"../../{TEST_ARCHITECTURE}/store/{TEST_DEFAULT_RELEASE_BUILD_HASH[:2]}/{TEST_DEFAULT_RELEASE_BUILD_HASH}/defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz"],
     }[pattern])
     @patch("alibuild_helpers.build.readlink", new=dummy_readlink)
     @patch("alibuild_helpers.build.banner", new=MagicMock(return_value=None))
@@ -243,9 +254,9 @@ class BuildTestCase(unittest.TestCase):
         mock_debug.side_effect = lambda *args: None
         mock_warning.side_effect = lambda *args: None
         mock_listdir.side_effect = lambda directory: {
-            "/sw/TARS/osx_x86-64/defaults-release": ["defaults-release-v1-1.osx_x86-64.tar.gz"],
-            "/sw/TARS/osx_x86-64/zlib": [],
-            "/sw/TARS/osx_x86-64/ROOT": [],
+            f"/sw/TARS/{TEST_ARCHITECTURE}/defaults-release": [f"defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz"],
+            f"/sw/TARS/{TEST_ARCHITECTURE}/zlib": [],
+            f"/sw/TARS/{TEST_ARCHITECTURE}/ROOT": [],
         }.get(directory, DEFAULT)
         os.environ["ALIBUILD_NO_ANALYTICS"] = "1"
 
@@ -257,7 +268,7 @@ class BuildTestCase(unittest.TestCase):
             docker=False,
             dockerImage=None,
             docker_extra_args=["--network=host"],
-            architecture="osx_x86-64",
+            architecture=TEST_ARCHITECTURE,
             workDir="/sw",
             pkgname=["root"],
             configDir="/alidist",
@@ -319,7 +330,7 @@ class BuildTestCase(unittest.TestCase):
         doBuild(args, mock_parser)
         mock_warning.assert_called_with("%s.sh contains a recipe, which will be ignored", "defaults-release")
         mock_debug.assert_called_with("Everything done")
-        mock_listdir.assert_called_with("/sw/TARS/osx_x86-64/ROOT")
+        mock_listdir.assert_called_with(f"/sw/TARS/{TEST_ARCHITECTURE}/ROOT")
         # We can't compare directly against the list of calls here as they
         # might happen in any order.
         mock_git_git.assert_has_calls(common_calls + [
@@ -406,8 +417,8 @@ class BuildTestCase(unittest.TestCase):
         self.assertNotIn("Extra", complete_initdotsh)
 
         # Dependencies must be loaded both for this build and for subsequent ones.
-        self.assertIn('. "$WORK_DIR/$ALIBUILD_ARCH_PREFIX"/zlib/v1.2.3-1/etc/profile.d/init.sh', setup_initdotsh)
-        self.assertIn('. "$WORK_DIR/$ALIBUILD_ARCH_PREFIX"/zlib/v1.2.3-1/etc/profile.d/init.sh', complete_initdotsh)
+        self.assertIn('. "$WORK_DIR/$ALIBUILD_ARCH_PREFIX"/zlib/v1.3.1-1/etc/profile.d/init.sh', setup_initdotsh)
+        self.assertIn('. "$WORK_DIR/$ALIBUILD_ARCH_PREFIX"/zlib/v1.3.1-1/etc/profile.d/init.sh', complete_initdotsh)
 
         # ROOT-specific variables must not be set during ROOT's build yet...
         self.assertNotIn("export ROOT_VERSION=", setup_initdotsh)

bitsorg/bits

tests/test_build.py
@@ -1,6 +1,7 @@
 from argparse import Namespace
 import os
 import os.path
+import platform
 import re
 import sys
 import unittest
@@ -11,6 +12,19 @@ from collections import OrderedDict
 
 from bits_helpers.utilities import parseRecipe, resolve_tag
 from bits_helpers.build import doBuild, storeHashes, generate_initdotsh
+
+# Determine architecture based on platform
+def get_test_architecture():
+    if sys.platform == 'darwin':
+        machine = platform.machine()
+        if machine == 'arm64':
+            return 'osx_arm64'
+        else:
+            return 'osx_x86-64'
+    else:
+        return 'slc7_x86-64'
+
+TEST_ARCHITECTURE = os.environ.get('ARCHITECTURE', get_test_architecture())
 
 
 TEST_DEFAULT_RELEASE = """\
@@ -23,7 +37,7 @@ TEST_DEFAULT_RELEASE_BUILD_HASH = "27ce49698e818e8efb56b6eff6dd785e503df341"
 
 TEST_ZLIB_RECIPE = """\
 package: zlib
-version: v1.2.3
+version: v1.3.1
 source: https://github.com/madler/zlib
 tag: master
 ---
@@ -32,7 +46,7 @@ make
 make install
 """
 TEST_ZLIB_GIT_REFS = "8822efa61f2a385e0bc83ca5819d608111b2168a\trefs/heads/master"
-TEST_ZLIB_BUILD_HASH = "8cd1f56c450f05ffbba3276bad08eae30f814999"
+TEST_ZLIB_BUILD_HASH = "4d6a75f214dc7931a2a7d5ba82ea0568e652cd84"
 
 TEST_ROOT_RECIPE = """\
 package: ROOT
@@ -71,7 +85,7 @@ TEST_ROOT_GIT_REFS = """\
 87b87c4322d2a3fad315c919cb2e2dd73f2154dc\trefs/heads/master
 f7b336611753f1f4aaa94222b0d620748ae230c0\trefs/heads/v6-08-00-patches
 f7b336611753f1f4aaa94222b0d620748ae230c0\trefs/tags/test-tag"""
-TEST_ROOT_BUILD_HASH = ("8ec3f41b6b585ef86a02e9c595eed67f34d63f08")
+TEST_ROOT_BUILD_HASH = ("1f3c771080f71b6c0d2e3d7a285698a20035da12")
 
 
 TEST_EXTRA_RECIPE = """\
@@ -88,18 +102,18 @@ f000\trefs/heads/master
 ba22\trefs/tags/v1
 ba22\trefs/tags/v2
 baad\trefs/tags/v3"""
-TEST_EXTRA_BUILD_HASH = ("5afae57bfc6a374e74c1c4427698ab5edebce0bc")
+TEST_EXTRA_BUILD_HASH = ("6e7bc4976abf77b558cf7faf575ec51670f8d0e5")
 
 
 GIT_CLONE_REF_ZLIB_ARGS = ("clone", "--bare", "https://github.com/madler/zlib",
                            "/sw/MIRROR/zlib", "--filter=blob:none"), ".", False
 GIT_CLONE_SRC_ZLIB_ARGS = ("clone", "-n", "https://github.com/madler/zlib",
-                           "/sw/SOURCES/zlib/v1.2.3/8822efa61f",
+                           "/sw/SOURCES/zlib/v1.3.1/8822efa61f",
                            "--dissociate", "--reference", "/sw/MIRROR/zlib", "--filter=blob:none"), ".", False
 GIT_SET_URL_ZLIB_ARGS = ("remote", "set-url", "--push", "origin", "https://github.com/madler/zlib"), \
-    "/sw/SOURCES/zlib/v1.2.3/8822efa61f", False
+    "/sw/SOURCES/zlib/v1.3.1/8822efa61f", False
 GIT_CHECKOUT_ZLIB_ARGS = ("checkout", "-f", "master"), \
-    "/sw/SOURCES/zlib/v1.2.3/8822efa61f", False
+    "/sw/SOURCES/zlib/v1.3.1/8822efa61f", False
 
 GIT_FETCH_REF_ROOT_ARGS = ("fetch", "-f", "--prune", "--filter=blob:none", "https://github.com/root-mirror/root", "+refs/tags/*:refs/tags/*",
                            "+refs/heads/*:refs/heads/*"), "/sw/MIRROR/root", False
@@ -143,9 +157,9 @@ def dummy_open(x, mode="r", encoding=None, errors=None):
                 "/sw/BUILD/%s/defaults-release/.build_succeeded" % TEST_DEFAULT_RELEASE_BUILD_HASH: (0, StringIO("0")),
                 "/sw/BUILD/%s/zlib/.build_succeeded" % TEST_ZLIB_BUILD_HASH: (0, StringIO("0")),
                 "/sw/BUILD/%s/ROOT/.build_succeeded" % TEST_ROOT_BUILD_HASH: (0, StringIO("0")),
-                "/sw/osx_x86-64/defaults-release/v1-1/.build-hash": (1, StringIO(TEST_DEFAULT_RELEASE_BUILD_HASH)),
-                "/sw/osx_x86-64/zlib/v1.2.3-local1/.build-hash": (1, StringIO(TEST_ZLIB_BUILD_HASH)),
-                "/sw/osx_x86-64/ROOT/v6-08-30-local1/.build-hash": (1, StringIO(TEST_ROOT_BUILD_HASH))
+                f"/sw/{TEST_ARCHITECTURE}/defaults-release/v1-1/.build-hash": (1, StringIO(TEST_DEFAULT_RELEASE_BUILD_HASH)),
+                f"/sw/{TEST_ARCHITECTURE}/zlib/v1.3.1-local1/.build-hash": (1, StringIO(TEST_ZLIB_BUILD_HASH)),
+                f"/sw/{TEST_ARCHITECTURE}/ROOT/v6-08-30-local1/.build-hash": (1, StringIO(TEST_ROOT_BUILD_HASH))
             }[x]
         except KeyError:
             return DEFAULT
@@ -163,17 +177,16 @@ def dummy_execute(x, **kwds):
     if re.match(".*ln -sfn.*TARS", s):
         return 0
     return {
-        "/bin/bash -e -x /sw/SPECS/osx_x86-64/defaults-release/v1-1/build.sh 2>&1": 0,
-        '/bin/bash -e -x /sw/SPECS/osx_x86-64/zlib/v1.2.3-local1/build.sh 2>&1': 0,
-        '/bin/bash -e -x /sw/SPECS/osx_x86-64/ROOT/v6-08-30-local1/build.sh 2>&1': 0,
+        f"/bin/bash -e -x /sw/SPECS/{TEST_ARCHITECTURE}/defaults-release/v1-1/build.sh 2>&1": 0,
+        f'/bin/bash -e -x /sw/SPECS/{TEST_ARCHITECTURE}/zlib/v1.3.1-local1/build.sh 2>&1': 0,
+        f'/bin/bash -e -x /sw/SPECS/{TEST_ARCHITECTURE}/ROOT/v6-08-30-local1/build.sh 2>&1': 0,
     }[s]
 
 
 def dummy_readlink(x):
     return {
-        "/sw/TARS/osx_x86-64/defaults-release/defaults-release-v1-1.osx_x86-64.tar.gz":
-        "../../osx_x86-64/store/%s/%s/defaults-release-v1-1.osx_x86-64.tar.gz" %
-        (TEST_DEFAULT_RELEASE_BUILD_HASH[:2], TEST_DEFAULT_RELEASE_BUILD_HASH)
+        f"/sw/TARS/{TEST_ARCHITECTURE}/defaults-release/defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz":
+        f"../../{TEST_ARCHITECTURE}/store/{TEST_DEFAULT_RELEASE_BUILD_HASH[:2]}/{TEST_DEFAULT_RELEASE_BUILD_HASH}/defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz"
     }[x]
 
 
@@ -225,13 +238,11 @@ class BuildTestCase(unittest.TestCase):
     @patch("os.listdir")
     @patch("bits_helpers.build.glob", new=lambda pattern: {
         "*": ["zlib"],
-        "/sw/TARS/osx_x86-64/store/%s/%s/*gz" % (TEST_DEFAULT_RELEASE_BUILD_HASH[:2],
-                                                 TEST_DEFAULT_RELEASE_BUILD_HASH): [],
-        "/sw/TARS/osx_x86-64/store/%s/%s/*gz" % (TEST_ZLIB_BUILD_HASH[:2], TEST_ZLIB_BUILD_HASH): [],
-        "/sw/TARS/osx_x86-64/store/%s/%s/*gz" % (TEST_ROOT_BUILD_HASH[:2], TEST_ROOT_BUILD_HASH): [],
-        "/sw/TARS/osx_x86-64/defaults-release/defaults-release-v1-1.osx_x86-64.tar.gz":
-        ["../../osx_x86-64/store/%s/%s/defaults-release-v1-1.osx_x86-64.tar.gz" %
-         (TEST_DEFAULT_RELEASE_BUILD_HASH[:2], TEST_DEFAULT_RELEASE_BUILD_HASH)],
+        f"/sw/TARS/{TEST_ARCHITECTURE}/store/{TEST_DEFAULT_RELEASE_BUILD_HASH[:2]}/{TEST_DEFAULT_RELEASE_BUILD_HASH}/*gz": [],
+        f"/sw/TARS/{TEST_ARCHITECTURE}/store/{TEST_ZLIB_BUILD_HASH[:2]}/{TEST_ZLIB_BUILD_HASH}/*gz": [],
+        f"/sw/TARS/{TEST_ARCHITECTURE}/store/{TEST_ROOT_BUILD_HASH[:2]}/{TEST_ROOT_BUILD_HASH}/*gz": [],
+        f"/sw/TARS/{TEST_ARCHITECTURE}/defaults-release/defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz":
+        [f"../../{TEST_ARCHITECTURE}/store/{TEST_DEFAULT_RELEASE_BUILD_HASH[:2]}/{TEST_DEFAULT_RELEASE_BUILD_HASH}/defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz"],
     }[pattern])
     @patch("bits_helpers.build.readlink", new=dummy_readlink)
     @patch("bits_helpers.build.banner", new=MagicMock(return_value=None))
@@ -244,9 +255,9 @@ class BuildTestCase(unittest.TestCase):
         mock_debug.side_effect = lambda *args: None
         mock_warning.side_effect = lambda *args: None
         mock_listdir.side_effect = lambda directory: {
-            "/sw/TARS/osx_x86-64/defaults-release": ["defaults-release-v1-1.osx_x86-64.tar.gz"],
-            "/sw/TARS/osx_x86-64/zlib": [],
-            "/sw/TARS/osx_x86-64/ROOT": [],
+            f"/sw/TARS/{TEST_ARCHITECTURE}/defaults-release": [f"defaults-release-v1-1.{TEST_ARCHITECTURE}.tar.gz"],
+            f"/sw/TARS/{TEST_ARCHITECTURE}/zlib": [],
+            f"/sw/TARS/{TEST_ARCHITECTURE}/ROOT": [],
         }.get(directory, DEFAULT)
         os.environ["BITS_NO_ANALYTICS"] = "1"
 
@@ -258,7 +269,7 @@ class BuildTestCase(unittest.TestCase):
             docker=False,
             dockerImage=None,
             docker_extra_args=["--network=host"],
-            architecture="osx_x86-64",
+            architecture=TEST_ARCHITECTURE,
             workDir="/sw",
             pkgname=["root"],
             configDir="/alidist",
@@ -321,7 +332,7 @@ class BuildTestCase(unittest.TestCase):
         doBuild(args, mock_parser)
         mock_warning.assert_called_with("%s.sh contains a recipe, which will be ignored", "defaults-release")
         mock_debug.assert_called_with("Everything done")
-        mock_listdir.assert_called_with("/sw/TARS/osx_x86-64/ROOT")
+        mock_listdir.assert_called_with(f"/sw/TARS/{TEST_ARCHITECTURE}/ROOT")
         # We can't compare directly against the list of calls here as they
         # might happen in any order.
         mock_git_git.assert_has_calls(common_calls + [
@@ -408,8 +419,8 @@ class BuildTestCase(unittest.TestCase):
         self.assertNotIn("Extra", complete_initdotsh)
 
         # Dependencies must be loaded both for this build and for subsequent ones.
-        self.assertIn('. "$WORK_DIR/$BITS_ARCH_PREFIX"/zlib/v1.2.3-1/etc/profile.d/init.sh', setup_initdotsh)
-        self.assertIn('. "$WORK_DIR/$BITS_ARCH_PREFIX"/zlib/v1.2.3-1/etc/profile.d/init.sh', complete_initdotsh)
+        self.assertIn('. "$WORK_DIR/$BITS_ARCH_PREFIX"/zlib/v1.3.1-1/etc/profile.d/init.sh', setup_initdotsh)
+        self.assertIn('. "$WORK_DIR/$BITS_ARCH_PREFIX"/zlib/v1.3.1-1/etc/profile.d/init.sh', complete_initdotsh)
 
         # ROOT-specific variables must not be set during ROOT's build yet...
         self.assertNotIn("export ROOT_VERSION=", setup_initdotsh)